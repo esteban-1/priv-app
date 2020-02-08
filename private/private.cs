51b32e806231f8a632c2ddee76706611177bd28e
mbz <36136687+esteban-1@users.noreply.github.com>
2020-02-08 14:55:55 -0500

    updates
    updates

---
+++ private/private.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace @private
+{
+    public partial class formprincipal : Form
+    {
+        private object managment;
+
+        public formprincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnsettings_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void formprincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnhome_Click(object sender, EventArgs e)
+        {
+        }
+        private void homemanagment(object managment)
+        {
+        }
+
+    }
+}
